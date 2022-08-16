2d2246dc81478ab5babb4a9ef4fc89e3159dff3e
Seondong Pyo <psd1390@real-iv.co.kr>
2022-08-16 12:57:02 +0900

    DI 테스트

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -1,21 +1,23 @@
 package core.di.factory;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 
+import java.lang.reflect.Constructor;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class BeanFactory {
     private static final Logger logger = LoggerFactory.getLogger(BeanFactory.class);
+    private final Set<Class<?>> preInstantiateBeans;
+    private final Map<Class<?>, Object> beans = Maps.newHashMap();
 
-    private Set<Class<?>> preInstanticateBeans;
-
-    private Map<Class<?>, Object> beans = Maps.newHashMap();
-
-    public BeanFactory(Set<Class<?>> preInstanticateBeans) {
-        this.preInstanticateBeans = preInstanticateBeans;
+    public BeanFactory(Set<Class<?>> preInstantiateBeans) {
+        this.preInstantiateBeans = preInstantiateBeans;
     }
 
     @SuppressWarnings("unchecked")
@@ -24,6 +26,36 @@ public class BeanFactory {
     }
 
     public void initialize() {
+        preInstantiateBeans.forEach(this::instantiate);
+    }
 
+    Object instantiate(Class<?> clazz) {
+        if (beans.containsKey(clazz)) {
+            return beans.get(clazz);
+        }
+
+        Constructor<?> constructor = BeanFactoryUtils.getInjectedConstructor(clazz);
+        Object instance = createBeanInstance(clazz, constructor);
+        beans.put(clazz, instance);
+        return instance;
+    }
+
+    private Object createBeanInstance(Class<?> clazz, Constructor<?> constructor) {
+        if (constructor == null) {
+            return BeanUtils.instantiateClass(clazz);
+        }
+        logger.debug("class = {}", clazz);
+        return instantiateConstructor(constructor);
+    }
+
+    private Object instantiateConstructor(Constructor<?> constructor) {
+        Class<?>[] parameterTypes = constructor.getParameterTypes();
+        List<Object> arguments = Lists.newArrayList();
+        for (Class<?> parameterType : parameterTypes) {
+            logger.debug("parameter type = {}", parameterType);
+            Class<?> concreteClass = BeanFactoryUtils.findConcreteClass(parameterType, preInstantiateBeans);
+            arguments.add(instantiate(concreteClass));
+        }
+        return BeanUtils.instantiateClass(constructor, arguments.toArray());
     }
 }
--- src/test/java/core/di/factory/BeanFactoryTest.java
+++ src/test/java/core/di/factory/BeanFactoryTest.java
@@ -15,21 +15,21 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-public class BeanFactoryTest {
+class BeanFactoryTest {
     private Reflections reflections;
     private BeanFactory beanFactory;
 
     @BeforeEach
     @SuppressWarnings("unchecked")
-    public void setup() {
+    void setup() {
         reflections = new Reflections("core.di.factory.example");
-        Set<Class<?>> preInstanticateClazz = getTypesAnnotatedWith(Controller.class, Service.class, Repository.class);
-        beanFactory = new BeanFactory(preInstanticateClazz);
+        Set<Class<?>> preInstantiateClasses = getTypesAnnotatedWith(Controller.class, Service.class, Repository.class);
+        beanFactory = new BeanFactory(preInstantiateClasses);
         beanFactory.initialize();
     }
 
     @Test
-    public void di() throws Exception {
+    void di() {
         QnaController qnaController = beanFactory.getBean(QnaController.class);
 
         assertNotNull(qnaController);
